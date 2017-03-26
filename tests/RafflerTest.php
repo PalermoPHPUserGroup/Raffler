6769349f8a94541d23e8c614b561255e3471fb62
enrico <enrico@startstudio.it>
2017-03-26 19:09:52 +0200

    Adding tests for Raffler

--- tests/RafflerTest.php
+++ tests/RafflerTest.php
@@ -7,19 +7,62 @@ include_once 'Raffler.php';
  */
 class RafflerTest extends PHPUnit_Framework_TestCase
 {
-    public function testCanInitRaffler()
+
+    /**
+     * @test
+     */
+    public function canInitRaffler()
     {
         $raffler = new Raffler();
 
         $this->assertInstanceOf(Raffler::class, $raffler);
     }
 
-    public function testCanAddCompetitors()
+    /**
+     * @test
+     */
+    public function canAddCompetitors()
     {
         $raffler = new Raffler();
         $raffler->addCompetitor('One');
         $raffler->addCompetitor('Two');
 
         $this->assertEquals(2, $raffler->getCompetitorsCount());
+    }
+
+    /**
+     * @test
+     */
+    public function canRunRaffle()
+    {
+        $filename = 'logs/log_' . date('Y-m-d') . '.log';
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
+
+        ob_start();
+        $raffler = new Raffler();
+        $raffler->addCompetitor('One');
+        $raffler->addCompetitor('One');
+        $raffler->run();
+        ob_clean();
+
+        $filename = 'logs/log_' . date('Y-m-d') . '.log';
+
+        $log = file_get_contents($filename);
+
+        $this->assertContains('There are 2 competitors:', $log);
+        $this->assertContains('- One', $log);
+        $this->assertContains('Let\'s turn the wheel...', $log);
+        $this->assertContains('One, One', $log);
+        $this->assertContains('I\'m sorry for One :(', $log);
+        $this->assertContains('Please try again next time', $log);
+        $this->assertContains('And the winner is...', $log);
+
+        $this->assertEquals(1, $raffler->getCompetitorsCount());
+
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
     }
 }
